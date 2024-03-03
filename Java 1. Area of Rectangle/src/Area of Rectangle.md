13af97160f0943e036fa4a95de89a445d11b16cf
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-03-03 20:22:57 +0300

    Added Area of Rectangle

---
+++ Java 1. Area of Rectangle/src/Area of Rectangle.md
@@ -0,0 +1,21 @@
+Area of Rectangle
+-
+Problem Description
+Create a program to find the area of a rectangle.
+
+The formula to compute the area of a rectangle is.
+
+    area = length * breadth
+
+- Take two integer inputs from the user and store them in length and breadth variables respectively.
+- Use the above formula to compute the area of the rectangle.
+
+Example
+-
+
+    Test Input
+    9
+    3
+-----
+    Expected Output
+    27
--- Java 1. Area of Rectangle/src/Main.java
+++ Java 1. Area of Rectangle/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        // take input for length and breadth
+        System.out.print("Enter length : ");
+        int length = input.nextInt();
+        System.out.print("Enter breadth : ");
+        int breadth = input.nextInt();
+
+        // calculate area of rectangle
+        int area = length * breadth;
+        System.out.println("Area : " + area);
+
+        input.close();
     }
 }
