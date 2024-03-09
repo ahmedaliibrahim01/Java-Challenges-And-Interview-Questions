26f642fa5e00a51d623b502b84269bf12270aff0
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-03-09 19:48:33 +0300

    Greates Factor of Numebr

---
+++ Java 01. Basics Challenges/Java 27. Greatest Factor of Number/README.md
@@ -0,0 +1,18 @@
+## Greatest Factor of Number (Using Loop)
+
+### Problem Description
+
+
+Create a program to print the greatest factor of a number besides itself using a loop.
+
+- Get an integer input and assign it to the number variable.
+- Create a for loop that runs from i = number - 1 to i = 1.
+- Inside the loop, check if number is perfectly divisible by i and break the loop.
+
+### Example
+
+    Test Input
+    14
+----
+    Expected Output
+    7
---
+++ Java 01. Basics Challenges/Java 27. Greatest Factor of Number/src/Main.java
@@ -0,0 +1,23 @@
+// Replace ___ with your code
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        // get input value for number
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        // run the loop from i = number - 1 to 1
+        for (___) {
+
+            // check if number is divisible by i
+            // if true, print the value of i and break the loop
+            if (number % i == 0) {
+                ___
+            }
+        }
+
+        input.close();
+    }
+}
