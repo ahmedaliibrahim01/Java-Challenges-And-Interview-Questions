5be82bb40095e6d5c427740b2a07eca1d63503f9
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-03-09 19:52:06 +0300

    created folder (Number Except Multiple of 3

---
+++ Java 01. Basics Challenges/Java 28. Numbers Except Multiple of 3/src/Main.java
@@ -0,0 +1,11 @@
+// Replace ___ with your code
+class Main {
+    public static void main(String[] args) {
+
+        // loop from i = 1 to 10
+        // inside the loop, check whether i is divisible by 3
+        // if true, skip the current iteration of the loop. Otherwise, print i
+        ___
+
+    }
+}
